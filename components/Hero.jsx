98ad6ec12c5a213de3594786eb61eec36dd54d89
joniel.Eom <joniel@naver.com>
2022-06-04 06:06:26 +0900

    초기화면 수정

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,14 +4,11 @@ import Logo from './Logo';
 
 const Hero = () => (
   <div className="hero my-5 text-center" data-testid="hero">
-    <Logo testId="hero-logo" />
-    <h1 className="mb-4" data-testid="hero-title">
-      Next.js Sample Project
-    </h1>
-
+    <h2 className="mb-4" data-testid="hero-title">
+      주의 말씀은 내발의 등이 빛이 나이다.
+    </h2>
     <p className="lead" data-testid="hero-lead">
-      This is a sample application that demonstrates an authentication flow for a Regular Web App, using{' '}
-      <a href="https://nextjs.org">Next.js</a>
+      주님의 연애편지가 많은 사람들의 손에 들려 읽혀지기를... 예수님의 이름올 기도드립니다. 아멘
     </p>
   </div>
 );
